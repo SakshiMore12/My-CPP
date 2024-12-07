c85a28e154f673c4bc1813d7154ea02a7b854407
SakshiMore12 <sakshithv1906@gmail.com>
2024-12-07 23:58:31 +0530

    Add files via upload

---
+++ 2_Print a number.cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  int number;
+  
+  cout<<"Enter a number:"; //Asking user for input
+  cin>>number;
+  
+  cout<<"You entered"<<" "<<number<<endl;
+  
+  return 0;
+}
